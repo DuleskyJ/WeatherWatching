cd342d94626a62d07b172843b0fc9646732446f7
Laptop <jdkyaz@gmail.com>
2024-02-06 22:24:40 -0700

    Trying to implement API in Javascript to show the current weather for searched city

--- script.js
+++ script.js
@@ -10,11 +10,6 @@
 //    .then((data) => console.log(data))
 // }
 
-citySubmit.addEventListener("click", function() {
-    findCity.value; 
-    console.log(findCity.value);
-    
-})
 
 
 
@@ -23,4 +18,35 @@ var currentWeather = document.querySelector("#currentWeather");
 var cityList = {};
 var citySubmit = document.querySelector("#citySubmit")
 
+citySubmit.addEventListener("click", function() {
+    findCity.value; 
+    console.log(findCity.value);
+    
+})
 
+$('#generatedCities').click(function(){
+    event.preventDefault();
+    var city = event.target.textContent;
+    geoCode(city); })
+
+function currentWeather(lat, lon) {
+    var requestURL = "https://api.openweathermap.org/data/2.5/forecast?lat="+ lat +'&lon='+ lon +'&appid=d92491cc84a33db5f345f1a39677bb71'
+    fetch(requestURL)
+    .then(function(response) {
+        return response.json(); 
+    }}
+    .then(function(data){
+        var tempDate = data.list[counter].dt_txt.slice('',10);
+        var year = tempDate.substr(0, 4);
+        var month = tempDate.substr(5,2);
+        var day = tempDate.substr(8, 2);
+        var date = month +"/"+ day + "/" + year;
+        console.log(data);
+currentWeather.children[1].textContent="Temp: "+data.main.temp+" °F";
+currentWeather.children[2].textContent="Wind: "+data.main.speed+" mph";
+currentWeather.children[3].textContent="Humidity: "+data.main.humidity+" %";   
+$('#currentDay').text 
+})
+
+
+var counter = 0;

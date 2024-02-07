f302163ac51bffc94fa4cfd3f49ff04d9c95a0c0
Laptop <jdkyaz@gmail.com>
2024-02-06 21:21:33 -0700

    Creating Java variables

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-function getWeather(city) { 
-    var weatherURL='https://api.openweathermap.org/data/2.5/weather?q='+ city +'&appid=d92491cc84a33db5f345f1a39677bb71&units=imperial' 
-    fetch(weatherURL)
-    .then(function(response){
-        return response.json();
-    }) 
-    .then((data) => console.log(data))
-}
-getWeather("New York");
+// MY API KEY that pulls from OpenWeather API: d92491cc84a33db5f345f1a39677bb71
+
+
+// function getWeather(city) { 
+//   var weatherURL='https://api.openweathermap.org/data/2.5/weather?q='+ city +'&appid=d92491cc84a33db5f345f1a39677bb71&units=imperial' 
+//    fetch(weatherURL)
+//    .then(function(response){
+//        return response.json();
+//    }) 
+//    .then((data) => console.log(data))
+// }
+
+citySubmit.addEventListener("click", function() {
+    findCity.value; 
+    console.log(findCity.value);
+    
+})
+
+
+
+var findCity = document.querySelector("#findCity");
+var currentWeather = document.querySelector("#currentWeather");
+var cityList = {};
+var citySubmit = document.querySelector("#citySubmit")
 
 
